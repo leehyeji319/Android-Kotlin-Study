92cb4760e2ec79617f7d3c081e9659df7aba568b
leehyeji319 <zzambbang@gmail.com>
2023-05-01 15:50:18 +0900

    feat: 생일 축하 카드 만들기

---
+++ Meterial3Example/app/src/main/java/com/example/meterial3example/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.meterial3example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meterial3example.ui.theme.Meterial3ExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Meterial3ExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Meterial3ExampleTheme {
+        Greeting("Android")
+    }
+}
